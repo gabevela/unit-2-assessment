8c865ac3cace66ff3fe3a897ce579cce5ae21b52
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-05-30 22:06:34 -0400

    unit 2

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('clicking the "add todo list" button')
+  res.render('index');
+});
 
-
+router.post('/', function(req, res, next) {
+  console.log('this is the "post" doing its thing')
   res.render('index');
 });
 
